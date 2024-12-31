5f3e3c95eac7ed761510bc30b00e257badd4b2bf
TONY-AKINLOSOTU Favour <favourlosotu@gmail.com>
2024-12-31 23:18:34 +0100

    Adjusted Whitenoise

--- APEM/settings.py
+++ APEM/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'staticfiles/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 STATICFILES_DIRS = (
