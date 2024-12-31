83e15956c9462d5b6d3fdec5a7baa6e5e0044119
TONY-AKINLOSOTU Favour <favourlosotu@gmail.com>
2024-12-31 19:45:30 +0100

    Changed Debug to True

--- APEM/settings.py
+++ APEM/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_oombv(tqrq1qa7@)q&@881ht=36=g5$%_x@@o%!sw18&b)($%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
