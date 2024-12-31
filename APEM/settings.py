bba40de25363ebad802474911ee4e4f28176671f
TONY-AKINLOSOTU Favour <favourlosotu@gmail.com>
2024-12-31 12:59:14 +0100

    Ignored database files and pycahe

--- APEM/settings.py
+++ APEM/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_oombv(tqrq1qa7@)q&@881ht=36=g5$%_x@@o%!sw18&b)($%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR/ "static"),
